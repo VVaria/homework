137632de873da18a4d777a885175cab7bdb93ab3
VVaria <2001varvara11@gmai.com>
2020-05-24 06:49:34 +0300

    Updates

---
+++ precache-manifest.9f5983b9957febca022ce687f9605ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab74f11f924210ef53ed58fd0fcd3172",
+    "url": "/homework/index.html"
+  },
+  {
+    "revision": "17f0332217fe3afa617d",
+    "url": "/homework/static/css/main.f56144c6.chunk.css"
+  },
+  {
+    "revision": "8030c85ff99069f9b950",
+    "url": "/homework/static/js/2.b80c4bec.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/homework/static/js/2.b80c4bec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f0332217fe3afa617d",
+    "url": "/homework/static/js/main.829ac472.chunk.js"
+  },
+  {
+    "revision": "753b4317e1890e17903d",
+    "url": "/homework/static/js/runtime-main.1c24986f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homework/precache-manifest.83f282d2e026d0d31b2ef7a6a99216be.js"
+  "/homework/precache-manifest.9f5983b9957febca022ce687f9605ebe.js"
 );
 
 self.addEventListener('message', (event) => {
